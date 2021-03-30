10b6308ce979e0f53fa6896949c4341f4f5c07cd
Puspa Widya <puspaparamitha24@gmail>
2021-03-30 11:59:49 +0700

    commit 12:00

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,78 +1,55 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState ,useEffect} from 'react';
 import DogList from '../src/components/Doglist'
 import FormDog from '../src/components/DogForm'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-class App extends React.Component{
-  constructor(props){
-    super(props)
 
-    this.state ={
-      dogData:[],
-      image:[]
-    }
+function App(){
+  const [dogData,setDogData] = useState([])
 
-  }
-  getDataFromApi =()=>{
+  useEffect(()=>{
     fetch('https://api.thedogapi.com/v1/breeds?limit=5&page=1')
     .then(res=>res.json())
-    .then(res =>{
-      console.log(res)
-      this.setState({
-        ...this.state,
-        dogData: res
+    .then(res => {
+      setDogData(res)
+      console.log(dogData)
+    })
+     .catch(err =>{
+        console.log(err)
       })
-    })
-    .catch(err =>{
-      console.log(err)
-    })
-  }
+    },[])
+  
 
-  //seperti created
-  componentDidMount(){
-    this.getDataFromApi()
-  }
-
-
-  addDog = (newDataDog)=>{
+   const addDog = (newDataDog)=>{
     console.log(newDataDog,'newdata')
-    let newDog = this.state.dogData.concat(newDataDog)
-    this.setState({
-      ...this.state,
-      dogData:newDog
-    })
+    let newDog = dogData.concat(newDataDog)
+    setDogData(newDog)
   }
 
 
-  render() {
-    const {dogData} = this.state
-    return(
-      <>  
-      <div>
-        <img className="hero-image" src="https://images.unsplash.com/photo-1537151641189-e685b67326c5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80" alt=""></img>
-        <div className="hero-text">
-            <h1>Are you ready for the cuteness?</h1>
-              <p>We are waiting you</p>
-             </div>
+  return(
+         <>  
+           <div>
+              <img className="hero-image" src="https://images.unsplash.com/photo-1537151641189-e685b67326c5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80" alt=""></img>
+                <div className="hero-text">
+                <h1>Are you ready for the cuteness?</h1>
+                <p>We are waiting you</p>
+              </div>
           </div>
-
           <div className="card-deck " id="cardDog" >
-          <div className="row">
+            <div className="row">
           {
-           dogData.map(dog =>{
-           return <DogList dogs={dog} key={dog.id} ></DogList>
-           })
-           }
+            dogData.map(dog =>{
+            return <DogList dogs={dog} key={dog.id} ></DogList>
+            })
+          }
+           </div>
           </div>
-          </div>
-      
-        <FormDog addDog={this.addDog}></FormDog>
-      </>
-    ) 
-  }
+          <FormDog addDog={addDog}></FormDog>
+          </>
+ )
 }
-
 
 export default App;
